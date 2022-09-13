e5da021724f8079b3aeb203e8fe4ac783c74c0bd
rstrauss127 <33469244+rstrauss127@users.noreply.github.com>
2022-09-13 13:47:52 -0400

    Update state_handler.py

--- state_handler.py
+++ state_handler.py
@@ -3,11 +3,10 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import json
 
-
-host = ""
-username =""
-password = ""
-database =""
+host = "ccrm-internal-dev-instance-1.c2q9cwd22amo.us-east-1.rds.amazonaws.com"
+username ="postgres"
+password = "postgres"
+database ="ccrmdev"
 
 conn = psycopg2.connect(
     host = host,
@@ -18,11 +17,12 @@ conn = psycopg2.connect(
 
 def lambda_handler(event, context):
     cur = conn.cursor(cursor_factory = RealDictCursor)
-    cur.execute("select * from state")
+    cur.execute(""" SELECT userroleid, userroledescription, permissionindicator, lastupdatedby, active from "Truist_Lookup"."Lookup_User_Role" """) ##need these quotes because database tables are capitalized 
     results = cur.fetchall()
     json_result = json.dumps(results)
     print(json_result)
     return json_result
+    
     
 
 #lambda_handler()
